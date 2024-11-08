bd0d37102442e204dea0447f1010c1ea0063d0e1
Taylor Evans <taylor.evans@Student.SL.On.Ca>
2024-11-08 14:24:08 -0500

    Initial commit - End of Lab 6

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup>
+  import AppHeader from './components/AppHeader.vue';
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  </header>
+
+  <main>
+    <AppHeader />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  background-color: var(--color-secondary);
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 130px;
+}
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<!-- When we set the background colour in AppHeader.vue
+ it only affects the area around the component, not the whole page.
+ Vue actively keeps component's styles separate so they don't 
+ affect other parts of the app. -->
+
+<template>
+    <body>
+        <h1>Hello world</h1>
+    </body>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+    body {
+        background-color: pink;
+    }
+
+    h1 {
+        color:aqua;
+    }
+</style>
